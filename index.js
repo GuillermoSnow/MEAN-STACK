5c3323c86c1cb60e9ee1ba17b344cd0f54710dc6
GuillermoSnow <memo.terrazas@gmail.com>
2018-01-17 19:22:59 -0500

    Fin

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ mongoose.connect(config.uri, (err) => {
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
 app.use('/authentication', authentication);
 app.use('/blogs',blogs);
 
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // Start Server: Listen on port 8080
